9d7b5e5890ac66338091f0f2c078f6a222e127e8
Rizkyvib <83052473+Rizkyvib@users.noreply.github.com>
2021-04-23 13:27:47 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Project Baru             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by Rizky Vib                      #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Rizky Vib                                   #"
+echo -e "# Allah                                         #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Owner VPS                                     #"
+echo -e "#                                               #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
